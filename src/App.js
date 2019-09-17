4178e5c64f7252a147f7d2e00455345b262fbeea
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2019-09-17 19:58:21 +0530

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
-  // React lifecycle method that's called immediately after the component is mounted to the DOM.
+  // React lifecycle method that's called immediately after the component is mounted to the DOM
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
@@ -18,9 +19,19 @@ class App extends Component {
   }
 
   render() {
+    // Destructuring i.e. setting two constants with value of the state
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
-      <div className="App">
-        <CardList monsters={this.state.monsters}/>
+      <div className='App'>
+        <input
+          type='search'
+          placeholder='Search monsters'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
