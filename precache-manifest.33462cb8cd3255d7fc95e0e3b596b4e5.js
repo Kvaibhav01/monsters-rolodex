bc8ed338bce16b629b1ff42a8e722489b3494a8f
Vaibhav Khulbe <khulbevaibhavdev@gmail.com>
2019-09-18 20:03:06 +0530

    Updates

---
+++ precache-manifest.33462cb8cd3255d7fc95e0e3b596b4e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f9cd2c962e3b205e0d6e0fd06fa998",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "74d098eda5a67e3c215b",
+    "url": "/monsters-rolodex/static/css/main.e7fcfeb1.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monsters-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "74d098eda5a67e3c215b",
+    "url": "/monsters-rolodex/static/js/main.ccc422a1.chunk.js"
+  },
+  {
+    "revision": "5f535753dcec63ef3476",
+    "url": "/monsters-rolodex/static/js/runtime~main.7352cc78.js"
+  }
+]);
